07e16a0cdda479660b9c74d3166ad6b96c5b93f3
rebecca505 <52841046+rebecca505@users.noreply.github.com>
2019-07-29 09:34:28 -0700

    Add files via upload

---
+++ Choose Your Own Adventure/Choose Your Own Adventure/CharacterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CharacterViewController.swift
+//  Choose Your Own Adventure
+//
+//  Created by GWC2 on 7/24/19.
+//  Copyright © 2019 GWC2. All rights reserved.
+//
+
+import UIKit
+
+class CharacterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Choose Your Own Adventure/Choose Your Own AdventureTests/Choose_Your_Own_AdventureTests.swift
@@ -0,0 +1,34 @@
+//
+//  Choose_Your_Own_AdventureTests.swift
+//  Choose Your Own AdventureTests
+//
+//  Created by GWC2 on 7/22/19.
+//  Copyright © 2019 GWC2. All rights reserved.
+//
+
+import XCTest
+@testable import Choose_Your_Own_Adventure
+
+class Choose_Your_Own_AdventureTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
